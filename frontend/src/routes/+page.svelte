0c970ffb627c584eaff1e22c9ff23627190be390
Stuart Lynn <stuart.lynn@gmail.com>
2025-02-03 23:11:27 +0000

    Typo

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class='h-screen flex flex-col gap-y-10 items-center justify-center'>
   <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Welcome to Comhairle</h1>
-  <p class="text-2xl">A platfrom about understanding what people want</p>
+  <p class="text-2xl">A platform about understanding what people want</p>
 
   {#await dataPromise}
     Loading data 
