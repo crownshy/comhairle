e22e856de278c6a8e7f38c96d0cb287cb492acc0
Stuart Lynn <stuart.lynn@gmail.com>
2025-02-03 23:12:11 +0000

    Removing api test and fixing typo

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-  import {getData} from "$lib/api"
-  let dataPromise= getData()
 </script>
 
 <div class='h-screen flex flex-col gap-y-10 items-center justify-center'>
   <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Welcome to Comhairle</h1>
   <p class="text-2xl">A platform about understanding what people want</p>
-
-  {#await dataPromise}
-    Loading data 
-  {:then data}
-    {data}
-  {:catch error}
-    <h2>Something went wrong</h2>
-    {error}
-  {/await}
 </div>
