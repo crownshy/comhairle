4bd2b85a97cf015f9f5e70efd10a6ff400f1a6f7
Stuart Lynn <stuart.lynn@gmail.com>
2025-02-25 21:42:24 +0000

    fleshing out tool page and adding report page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,13 +2,13 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
-	import NavBar from "$lib/components/NavBar.svelte"
+	import NavBar from '$lib/components/NavBar.svelte';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
 	<NavBar />
-	<div class='max-w-screen px-20 pt-40 mb-16'>
+	<div class="mx-auto mb-16 w-full max-w-[1300px] px-20 pt-40">
 		{@render children()}
 	</div>
 </ParaglideJS>
