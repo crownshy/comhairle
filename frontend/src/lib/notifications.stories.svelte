918144439dd91b0a88498943871e3a8a534da056
ddouglascarr <>
2025-02-27 10:57:26 +1100

    Adding priority, duration and action options to notification manager

--- frontend/src/lib/notifications.stories.svelte
+++ frontend/src/lib/notifications.stories.svelte
@@ -7,11 +7,64 @@
 	});
 </script>
 
-<Story name="local notification">
+<Story name="default prority notification">
 	<NotificationsToaster closeButton />
 	<button
 		onclick={() => {
-			notifications.send('I am a toast');
-		}}>Show toast</button
+			notifications.send({ message: 'I am a toast' });
+		}}>Send notification</button
+	>
+</Story>
+
+<Story name="info prority notification">
+	<NotificationsToaster closeButton />
+	<button
+		onclick={() => {
+			notifications.send({ priority: 'INFO', message: 'The sky is blue' });
+		}}>Send notification</button
+	>
+</Story>
+
+<Story name="warning prority notification">
+	<NotificationsToaster closeButton />
+	<button
+		onclick={() => {
+			notifications.send({ priority: 'WARNING', message: 'The sky is turning grey' });
+		}}>Send notification</button
+	>
+</Story>
+
+<Story name="error prority notification">
+	<NotificationsToaster closeButton />
+	<button
+		onclick={() => {
+			notifications.send({ priority: 'ERROR', message: 'It is raining' });
+		}}>Send notification</button
+	>
+</Story>
+
+<Story name="success prority notification">
+	<NotificationsToaster closeButton />
+	<button
+		onclick={() => {
+			notifications.send({ priority: 'SUCCESS', message: 'Your umbrella is up' });
+		}}>Send notification</button
+	>
+</Story>
+
+<Story name="notification with action">
+	<NotificationsToaster closeButton />
+	<button
+		onclick={() => {
+			notifications.send({
+				message: 'more notify',
+				action: {
+					label: 'click me',
+					onClick: () => {
+						notifications.send({ message: 'you can notify from a notification' });
+					}
+				}
+			});
+		}}>Send notification</button
 	>
 </Story>
--- frontend/src/lib/notifications.svelte.ts
+++ frontend/src/lib/notifications.svelte.ts
@@ -1,12 +1,13 @@
-import { toast } from 'svelte-sonner';
+import { toast, type ExternalToast } from 'svelte-sonner';
 export { Toaster as NotificationsToaster } from '$lib/components/ui/sonner';
 
 class NotificationsManager {
-	private static DURATION = 60000;
+	private static DEFAULT_DURATION = 60000;
 
-	public send(msg: string) {
-		toast(msg, {
-			duration: NotificationsManager.DURATION
+	public send(opts: SendOpts) {
+		this.toastFn(opts.priority)(opts.message, {
+			duration: opts.duration || NotificationsManager.DEFAULT_DURATION,
+			action: opts.action
 		});
 	}
 
@@ -22,14 +23,14 @@ class NotificationsManager {
 		// }, 6000);
 	}
 
-	private sendServerNotification(msg: string, id: string) {
-		toast(msg, {
-			duration: NotificationsManager.DURATION,
+	private sendServerNotification(opts: SendServerNotificationOpts) {
+		this.toastFn(opts.priority)(opts.message, {
+			duration: opts.duration || NotificationsManager.DEFAULT_DURATION,
 			onDismiss: () => {
-				this.ack(id);
+				this.ack(opts.id);
 			},
 			onAutoClose: () => {
-				this.ack(id);
+				this.ack(opts.id);
 			}
 		});
 	}
@@ -38,14 +39,44 @@ class NotificationsManager {
 		console.log(`acknowlegding server notification ${id}`);
 		// TODO: implement
 	}
+
+	private toastFn(priority?: NotificationPriorities) {
+		switch (priority) {
+			case 'ERROR': {
+				return toast.error;
+			}
+			case 'INFO': {
+				return toast.info;
+			}
+			case 'SUCCESS': {
+				return toast.success;
+			}
+			case 'WARNING': {
+				return toast.warning;
+			}
+			default: {
+				return toast;
+			}
+		}
+	}
 }
+
+type SendOpts = BaseOpts & { action?: ExternalToast['action'] };
+type SendServerNotificationOpts = BaseOpts & { id: string };
+
+type BaseOpts = {
+	message: string;
+	priority?: NotificationPriorities;
+	duration?: number;
+};
+export type NotificationPriorities = 'INFO' | 'WARNING' | 'ERROR' | 'SUCCESS';
 
 // `notifications` is the central notifications manager for the project. It can
 // listen for notifications from the server, and it can send notifications
 // within the local app.
 //
 // To send a notification within the app:
-//   notifications.send("my message");
+//   notifications.send({ message: "my message" });
 //
 // To listen for notifications from the server (should be done once):
 //   $effect(() => { notifications.listen(); });
