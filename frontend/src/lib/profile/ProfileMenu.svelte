858eabd0907f2fa58285de078fbf59db92f82e9f
Stuart Lynn <stuart.lynn@gmail.com>
2025-03-05 11:04:52 +0000

    Adding basic avatar with fallback to profile button

--- frontend/src/lib/profile/ProfileMenu.svelte
+++ frontend/src/lib/profile/ProfileMenu.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
+  import * as Avatar from "$lib/components/ui/avatar";
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import { Button } from '$lib/components/ui/form';
 	import { buttonVariants } from '$lib/components/ui/button';
 	import LoginButtons from './LoginButtons.svelte';
 	const { user } = $props();
+	let user_initals = $derived(user?.username.split(/\s/).map(s=>s.charAt(0).toUpperCase()).join(""));
 </script>
 
 {#if user}
 	<DropdownMenu.Root>
 		<DropdownMenu.Trigger class={buttonVariants({ variant: 'outline' })}
-			>{user.username}</DropdownMenu.Trigger
+			>
+				<Avatar.Root class="h-6 w-6 mr-4">
+					{#if user.avatar_url}
+					  <Avatar.Image src={user.avatar_url} alt="@shadcn" />
+				  {/if}
+				  <Avatar.Fallback>{user_initals}</Avatar.Fallback>
+				</Avatar.Root>
+				{user.username}
+			</DropdownMenu.Trigger
 		>
 		<DropdownMenu.Content>
 			<DropdownMenu.Group>
