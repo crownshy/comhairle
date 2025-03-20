1fdc7273a06d694c1d941be19fbf52324b6f3f51
Daniel Carr <d.douglas.carr@gmail.com>
2025-03-20 19:57:22 +1100

    #41 fixing overflow on conversation cards (#50)
    Co-authored-by: ddouglascarr <>

--- frontend/src/lib/components/ConversationCard.svelte
+++ frontend/src/lib/components/ConversationCard.svelte
@@ -3,22 +3,17 @@
 	import * as Card from '$lib/components/ui/card/index';
 	import { Button } from '$lib/components/ui/button';
 	let { conversation }: { conversation: Conversation } = $props();
-	console.log("conversation", conversation)
 </script>
 
-<Card.Root class="h-[400px] w-[350px]">
-	<Card.Header>
-		<Card.Title>{conversation.title}</Card.Title>
-		<Card.Description>{conversation.description}</Card.Description>
+<Card.Root class="flex h-[400px] w-[350px] flex-col">
+	<Card.Header class="grow">
+		<Card.Title class="truncate">{conversation.title}</Card.Title>
+		<Card.Description class="line-clamp-4">{conversation.description}</Card.Description>
 	</Card.Header>
-	<Card.Content>
-		<form>
-			<div class="grid w-full items-center gap-4">
-				<img src={conversation.image_url} alt={conversation.title} />
-			</div>
-		</form>
+	<Card.Content class="grow">
+		<img class="m-auto h-32" src={conversation.image_url} alt={conversation.title} />
 	</Card.Content>
-	<Card.Footer class="flex justify-between">
+	<Card.Footer class="flex grow-0 justify-between">
 		{#if conversation.is_complete}
 			<Button href={`/conversations/${conversation.id}/report`}>View Report</Button>
 		{:else}
