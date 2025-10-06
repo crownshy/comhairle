2ef4dfc83cdfb0a605f6d838b488994855b42c7a
Stuart Lynn <stuart.lynn@gmail.com>
2025-10-06 15:50:15 +0100

    Admin checking quick fix

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -6,7 +6,12 @@ export const load: LayoutLoad = async ({ url, data }) => {
 	let token = data.token;
 	let user = data.user;
 	const api = createApiClient(url.origin + "/api", token, browser ? "client" : "server");
-	const userRoles = await api.GetUserRoles();
+	try {
+		const userRoles = await api.GetUserRoles();
+		return { api, user, userRoles };
+	}
+	catch (e) {
+		return { api, user };
+	}
 
-	return { api, user, userRoles };
 };
