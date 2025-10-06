f989cc9324bee54b545037983cdfa423ac26bb26
Stuart Lynn <stuart.lynn@gmail.com>
2025-10-06 12:09:59 +0100

    Making the common step config a modal

--- frontend/src/lib/components/CommonStepConfig/CommonStepConfig.svelte
+++ frontend/src/lib/components/CommonStepConfig/CommonStepConfig.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Input } from '$lib/components/ui/input';
 	import TextArea from '$lib/components/ui/textarea/textarea.svelte';
+	import { Button } from '$lib/components/ui/button';
 	import * as Form from '../ui/form/';
+	import * as Dialog from '$lib/components/ui/dialog';
 	import { invalidateAll } from '$app/navigation';
 	import { notifications } from '$lib/notifications.svelte';
 	import { zodClient } from 'sveltekit-superforms/adapters';
@@ -14,6 +16,8 @@
 		conversation_id: string;
 		step: WorkflowStep;
 	};
+
+	let open = $state(false);
 
 	let { step, conversation_id }: Props = $props();
 
@@ -42,37 +46,57 @@
 			});
 			invalidateAll();
 			notifications.send({ message: 'Updated workflow step', priority: 'INFO' });
+			open = false;
 		} catch (e) {
 			notifications.send({ message: 'Failed to update workflow step', priority: 'ERROR' });
 		}
 	}
 </script>
 
-<form method="POST" onsubmit={updateStep} class="mt-10 flex flex-col gap-y-5" use:enhance>
-	<Form.Field form={commonStepForm} name="name">
-		<Form.Control>
-			{#snippet children({ props })}
-				<Form.Label>Name</Form.Label>
-				<Input {...props} bind:value={$form.name} />
-			{/snippet}
-		</Form.Control>
-		<Form.Description class="text-black"
-			>The name of the step that will be shown to participants.</Form.Description
-		>
-		<Form.FieldErrors />
-	</Form.Field>
-	<Form.Field form={commonStepForm} name="description">
-		<Form.Control>
-			{#snippet children({ props })}
-				<Form.Label>Description</Form.Label>
-				<TextArea class="bg-white" {...props} bind:value={$form.description} />
-			{/snippet}
-		</Form.Control>
-		<Form.Description class="text-black"
-			>A description of this step that will inform users of it's intent.</Form.Description
-		>
-		<Form.FieldErrors />
-	</Form.Field>
+<div class="mb-10 flex flex-row items-start justify-between">
+	<div class="flex flex-col gap-2">
+		<h2 class="text-2xl">{step.name}</h2>
+		<p>{step.description}</p>
+	</div>
+	<Dialog.Root bind:open>
+		<Dialog.Trigger><Button variant="secondary">Edit Metadata</Button></Dialog.Trigger>
 
-	<Form.Button variant="secondary" class="my-5" disabled={$submitting}>Submit</Form.Button>
-</form>
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Edit the metadata?</Dialog.Title>
+				<Dialog.Description>
+					This is the name and description that will be shown to participants when they get to this
+					step
+				</Dialog.Description>
+			</Dialog.Header>
+			<form method="POST" onsubmit={updateStep} class="mt-10 flex flex-col gap-y-5" use:enhance>
+				<Form.Field form={commonStepForm} name="name">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label>Name</Form.Label>
+							<Input {...props} bind:value={$form.name} />
+						{/snippet}
+					</Form.Control>
+					<Form.Description class="text-black"
+						>The name of the step that will be shown to participants.</Form.Description
+					>
+					<Form.FieldErrors />
+				</Form.Field>
+				<Form.Field form={commonStepForm} name="description">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label>Description</Form.Label>
+							<TextArea class="bg-white" {...props} bind:value={$form.description} />
+						{/snippet}
+					</Form.Control>
+					<Form.Description class="text-black"
+						>A description of this step that will inform users of it's intent.</Form.Description
+					>
+					<Form.FieldErrors />
+				</Form.Field>
+
+				<Form.Button variant="default" class="my-5" disabled={$submitting}>Submit</Form.Button>
+			</form>
+		</Dialog.Content>
+	</Dialog.Root>
+</div>
