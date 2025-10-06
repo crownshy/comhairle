a53be53009b8d26ad353eb46dfba4acb3e731d59
Stuart Lynn <stuart.lynn@gmail.com>
2025-10-06 15:29:11 +0100

    NavBar admin indicator fix

--- frontend/src/routes/(public)/+layout.svelte
+++ frontend/src/routes/(public)/+layout.svelte
@@ -5,12 +5,14 @@
 
 	let { children, data }: LayoutProps = $props();
 	let isAdmin = $derived(
-		data.userRoles.find((ur) => ur.resource === 'Site')?.roles.includes('Admin')
+		data.userRoles
+			? data.userRoles.find((ur) => ur.resource === 'Site')?.roles.includes('Admin')
+			: false
 	);
 </script>
 
 <div class="flex w-full flex-col">
-	<NavBar user={data.user} isAdmin />
+	<NavBar user={data.user} {isAdmin} />
 	<div class="mx-auto min-h-screen w-full max-w-[1300px] grow px-4 md:px-20">
 		{@render children()}
 	</div>
