19aeb0d8e073d8bfbf304a5044395c3f4175dd1d
Stuart Lynn <stuart.lynn@gmail.com>
2025-10-06 11:54:56 +0100

    Home page padding on welcome message

--- frontend/src/routes/(public)/+page.svelte
+++ frontend/src/routes/(public)/+page.svelte
@@ -3,12 +3,14 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<div class="flex h-full flex-col items-center justify-center gap-y-10 p-10">
-	<h1
-		class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:text-left lg:text-5xl"
-	>
-		{m.welcome_to_comhairle()}
-	</h1>
-	<p class="text-center text-2xl md:text-left">{m.a_platfrom_about_what_people_want()}</p>
-	<Button size="lg" href="/conversations">{m.explore_conversations()}</Button>
+<div class="flex h-full flex-col items-center justify-around pt-50">
+	<div class="my-auto flex flex-col items-center justify-center gap-y-10 p-10">
+		<h1
+			class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:text-left lg:text-5xl"
+		>
+			{m.welcome_to_comhairle()}
+		</h1>
+		<p class="text-center text-2xl md:text-left">{m.a_platfrom_about_what_people_want()}</p>
+		<Button size="lg" href="/conversations">{m.explore_conversations()}</Button>
+	</div>
 </div>
