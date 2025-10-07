e17c245cd02f1d01aacf648233cd945a21c2a6d9
Stuart Lynn <stuart.lynn@gmail.com>
2025-10-07 10:46:28 +0100

    Fixing redirect bug

--- frontend/src/lib/urls.ts
+++ frontend/src/lib/urls.ts
@@ -7,7 +7,7 @@ export function conversation_url(conversation_id: string) {
 }
 
 export function manage_conversation_url(conversation_id: string) {
-	return `/admin/conversations/${conversation_id}/config`
+	return `/admin/conversations/${conversation_id}/configure`
 }
 
 export function workflow_url(conversation_id: string, workflow_id: string) {
