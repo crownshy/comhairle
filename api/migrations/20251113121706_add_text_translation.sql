f96168429775591404691b6045655de8a92966e1
Stuart Lynn <stuart.lynn@gmail.com>
2025-11-13 14:42:33 +0000

    Stubbing out translations

---
+++ api/migrations/20251113121706_add_text_translation.sql
@@ -0,0 +1,12 @@
+-- Add migration script here
+CREATE TABLE TEXT_TRANSLATION(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	content_id uuid References TEXT_CONTENT NOT NULL, 
+	locale TEXT NOT NULL,
+	content TEXT NOT NULL DEFAULT "",
+	ai_generated BOOLEAN DEFAULT FALSE,
+	requires_validation BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()	
+)-- Add migration script here
+
