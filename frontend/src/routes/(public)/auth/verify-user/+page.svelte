9df0ec042021d90872602c47115f41d29fdb5c10
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-11-19 15:26:55 +0000

    196 frontend email verification pages
    Actions:
    
    + build initial templates for verification pages
    
    Motivation:
    
    + email verification user pages

---
+++ frontend/src/routes/(public)/auth/verify-user/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { AuthPage } from '$lib/profile';
+	import type { PageData } from './$types';
+	import VerifyUser from './verify-user.svelte';
+	let { data }: PageData = $props();
+	let token = $derived(data.token);
+</script>
+
+<AuthPage>
+	<VerifyUser {token} />
+</AuthPage>
---
+++ frontend/src/routes/(public)/auth/verify-user/verify-user.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let { token } = $props();
+
+	let verifiedPromise = false; // TODO: make request to verify token
+</script>
+
+<section class="">
+	{#await verifiedPromise}
+		<h1 class="mb-2 text-xl font-bold">Verifying your email</h1>
+		<p class="mb-4">Click on the verification link we have sent to your email address.</p>
+	{:then}
+		<h1 class="mb-2 text-xl font-bold">Successfully verified your email</h1>
+		<p class="mb-4">
+			Please <a href="/auth/login" class="underline">log in</a> to your verified account to join a conversation.
+		</p>
+	{:catch}
+		<h1 class="mb-2 text-xl font-bold">Unable to verify your email address</h1>
+		<p class="mb-4">
+			Please click the link in your email again or
+			<button type="button" class="underline" onclick={() => alert('Resend verification email')}>
+				<!-- TODO: -->
+				Send again.
+			</button>
+		</p>
+	{/await}
+</section>
