0bbd6b03b70533a217476604f3c68282912eb0f8
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-11-19 15:26:55 +0000

    196 frontend email verification pages
    Actions:
    
    + build initial templates for verification pages
    
    Motivation:
    
    + email verification user pages

---
+++ frontend/src/routes/(public)/auth/verification-message/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { AuthPage } from '$lib/profile';
+	import VerificationMessage from './verification-message.svelte';
+</script>
+
+<AuthPage>
+	<VerificationMessage />
+</AuthPage>
---
+++ frontend/src/routes/(public)/auth/verification-message/verification-message.svelte
@@ -0,0 +1,10 @@
+<section class="">
+	<h1 class="text-xl font-bold mb-2">We have sent you an email</h1>
+	<p class="mb-4">Click on the verification link we have sent to your email address.</p>
+	<p class="text-sm">
+		Didn&apos;t receive the email yet? Check your spam folder or
+		<button type="button" class="underline" onclick={() => alert('Resend verification email')}> <!-- TODO: -->
+			Send again.
+		</button>
+	</p>
+</section>
