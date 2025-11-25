63fffdbeb4611232861fe1a7dbf0244ddc603825
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-11-25 16:20:35 +0000

    196 verify user refactor
    Actions:
    
    + remove unnecessary svelte component
    + move token verification to +page.ts
    + update success message to route back to conversation
    
    Motivation:
    
    + simplify route

--- frontend/src/routes/(public)/auth/verify-user/+page.svelte
+++ frontend/src/routes/(public)/auth/verify-user/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
 	import { AuthPage } from '$lib/profile';
-	import type { PageData } from './$types';
-	import VerifyUser from './verify-user.svelte';
-	let { data }: PageData = $props();
-	let token = $derived(data.token);
+	import type { PageProps } from './$types';
+	import * as m from '$lib/paraglide/messages';
+
+	let { data }: PageProps = $props();
+	let errorMessage = $derived(data.errorMessage);
 </script>
 
 <AuthPage>
-	<VerifyUser {token} />
+	<section>
+		{#if errorMessage}
+			<h1 class="mb-2 text-xl font-bold">{m.verify_user_error_heading()}</h1>
+			<p class="mb-4">
+				{@html errorMessage}
+			</p>
+		{:else}
+			<h1 class="mb-2 text-xl font-bold">{m.verify_user_success_heading()}</h1>
+			<p class="mb-4">{@html m.verify_user_success_body()}</p>
+		{/if}
+	</section>
 </AuthPage>
