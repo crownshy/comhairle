de5f4fcc862830f7433e737cc941be1a94306c3e
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-11-25 09:24:24 +0000

    196 verification message translation
    Actions:
    
    + add verification message text to paraglide and provide translations
    
    Motivation:
    
    + allow paraglide to translate hard coded text to supported languages

--- frontend/src/routes/(public)/auth/verification-message/verification-message.svelte
+++ frontend/src/routes/(public)/auth/verification-message/verification-message.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { apiClient } from '$lib/api/client';
 	import { toast } from 'svelte-sonner';
+	import * as m from '$lib/paraglide/messages';
 
 	let { user } = $props();
 
@@ -9,25 +10,26 @@
 	async function resendVerificationEmail() {
 		try {
 			await apiClient.ResendVerificationEmail({ user });
-			toast.success('Verification email re-sent. Please check your inbox.');
+			toast.success(m.verification_email_sent());
 		} catch (e) {
-			responseMessage = 'Something went wrong re-sending verification email.';
+			responseMessage = m.verify_error_message();
 			if (!user) {
-				responseMessage +=
-					'\nPlease try logging in. You will be notified at a later date to re-attempt email verification.';
+				responseMessage += m.verify_missing_user();
 			}
 		}
 	}
 </script>
 
 <section class="">
-	<h1 class="mb-2 text-xl font-bold">We have sent you an email</h1>
-	<p class="mb-4">Click on the verification link we have sent to your email address.</p>
+	<h1 class="mb-2 text-xl font-bold">{m.verification_message_heading()}</h1>
+	<p class="mb-4">{m.verification_message_body()}</p>
 	{#if responseMessage}
 		<p class="text-destructive mb-4 text-sm whitespace-break-spaces">{responseMessage}</p>
 	{/if}
 	<p class="text-sm">
-		Didn&apos;t receive the email yet? Check your spam folder or
-		<button type="button" class="underline" onclick={resendVerificationEmail}> Send again. </button>
+		{m.verification_email_missing()}
+		<button type="button" class="underline" onclick={resendVerificationEmail}>
+			{m.send_again()}.
+		</button>
 	</p>
 </section>
