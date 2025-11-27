031d78113196ce98a5bbbd87582769057aef6d77
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-11-27 12:24:52 +0000

    196 loading state
    Actions:
    
    + adjust loading / error state on verify token page
    
    Motivation:
    
    + error state was always being rendered before success state

--- frontend/src/routes/(public)/auth/verify-user/+page.ts
+++ frontend/src/routes/(public)/auth/verify-user/+page.ts
@@ -6,18 +6,24 @@ export const load: PageLoad = async ({ url }) => {
 	const token = url.searchParams.get('token') ?? '';
 
 	let errorMessage: string | null = null;
+	let status: string | null = null;
 
-	try {
-		await apiClient.VerifyEmailToken({ token });
-	} catch (e) {
-		if (e.response?.status === 409) {
-			errorMessage = m.verify_user_conflict_error_body();
-		} else {
-			errorMessage = m.verify_user_error_body();
+	if (token && typeof window !== 'undefined') {
+		try {
+			await apiClient.VerifyEmailToken({ token });
+			status = 'success';
+		} catch (e) {
+			status = 'error';
+			if (e.response?.status === 409) {
+				errorMessage = m.verify_user_conflict_error_body();
+			} else {
+				errorMessage = m.verify_user_error_body();
+			}
 		}
 	}
 
 	return {
+		status,
 		errorMessage
 	};
 };
