a4e1d264608236c32278e20ba6a58152773c7502
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-11-27 10:21:57 +0000

    197 password reset pages
    Actions:
    
    + add pages and translated text for password reset flow
    
    Motivation:
    
    + password reset

---
+++ frontend/src/routes/(public)/auth/password-reset/create/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { AuthPage } from '$lib/profile';
+	import * as m from '$lib/paraglide/messages';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<AuthPage>
+	<section>
+		<form class="space-y-4">
+			<h1 class="text-xl font-bold">{m.create_new_password_heading()}</h1>
+			<Label>{m.new_password()}</Label>
+			<Input />
+			<!-- TODO: use PasswordInput from other branch -->
+			<Label>{m.confirm_password()}</Label>
+			<Input />
+			<!-- TODO: use PasswordInput from other branch -->
+			<Button type="button">{m.submit()}</Button>
+		</form>
+	</section>
+</AuthPage>
