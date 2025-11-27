c9eff725ec46bc0594574cee42f41e8c20ab5b9b
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-11-27 10:21:57 +0000

    197 password reset pages
    Actions:
    
    + add pages and translated text for password reset flow
    
    Motivation:
    
    + password reset

---
+++ frontend/src/routes/(public)/auth/password-reset/sent/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { AuthPage } from '$lib/profile';
+	import * as m from '$lib/paraglide/messages';
+	import { MailCheck } from 'lucide-svelte';
+</script>
+
+<AuthPage>
+	<section class="space-y-4">
+		<div class="flex justify-center">
+			<MailCheck class="h-12 w-12" />
+		</div>
+		<h1 class="text-xl font-bold">{m.password_reset_instructions_heading()}</h1>
+		<p class="text-muted-foreground text-sm">{m.password_reset_instructions_body()}</p>
+	</section>
+</AuthPage>
