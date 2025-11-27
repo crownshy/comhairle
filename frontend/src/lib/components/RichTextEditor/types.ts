3df385d44f79188ff9a1270dba66ce97171dda15
Daniela Todorova <43169425+dtodorowa@users.noreply.github.com>
2025-11-27 07:46:19 +0000

    remove type that isn't needed yet

--- frontend/src/lib/components/RichTextEditor/types.ts
+++ frontend/src/lib/components/RichTextEditor/types.ts
@@ -4,18 +4,3 @@ export const CONTENT_TYPES = {
 } as const;
 
 export type ContentType = typeof CONTENT_TYPES[keyof typeof CONTENT_TYPES];
-
-export type ActiveStates = {
-	bold: boolean;
-	italic: boolean;
-	strike: boolean;
-	code: boolean;
-	underline: boolean;
-	link: boolean;
-	bulletList: boolean;
-	orderedList: boolean;
-	blockquote: boolean;
-	heading: 'p' | '1' | '2' | '3';
-	textAlign: 'left' | 'center' | 'right' | 'justify';
-};
-
