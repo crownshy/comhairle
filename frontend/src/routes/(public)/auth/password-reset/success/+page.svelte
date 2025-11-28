d001424163b9df34c1a2bcfa9ce3a2f58bd3f5c9
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-11-28 16:10:49 +0000

    197 password reset update frontend
    Actions:
    
    + add functionality to update page to submit updated password to api in
      reset flow
    + add success page that navigates user back to login page
    
    Motivation:
    
    + password reset flow

---
+++ frontend/src/routes/(public)/auth/password-reset/success/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import AuthPage from '$lib/profile/AuthPage.svelte';
+	import * as m from '$lib/paraglide/messages';
+</script>
+
+<AuthPage>
+	<section class="space-y-4">
+		<h1 class="text-xl font-bold">{m.password_reset_success_heading()}</h1>
+		<p class="text-muted-foreground mb-4 text-sm">{@html m.password_reset_success_body()}</p>
+	</section>
+</AuthPage>
