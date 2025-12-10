a44e620e8b825c4b8ec85b9bb763a4f063e0d13a
Daniela Todorova <43169425+dtodorowa@users.noreply.github.com>
2025-12-10 14:12:43 +0000

    revert storybook changes that are in a separate pr

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,28 +1,14 @@
-import type { Preview } from '@storybook/sveltekit';
-import { withThemeByClassName } from '@storybook/addon-themes';
-
-// Import your app's global CSS (includes design tokens)
-import '../src/app.css';
+import type { Preview } from '@storybook/sveltekit'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+       color: /(background|color)$/i,
+       date: /Date$/i,
       },
     },
   },
-  decorators: [
-    withThemeByClassName({
-      themes: {
-        'Figma': 'figma',
-        'Scottish Government': 'scot-gov',
-        'Comhairle': 'comhairle',
-      },
-      defaultTheme: 'Comhairle',
-    }),
-  ],
 };
 
 export default preview;
