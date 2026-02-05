efcb8627840a619b827cc38e1f391b5053c53680
Daniela Todorova <43169425+dtodorowa@users.noreply.github.com>
2026-02-05 09:58:57 +0000

    update language selector chevron icon styling (make it match text)

--- frontend/src/lib/components/LanguageSelector.svelte
+++ frontend/src/lib/components/LanguageSelector.svelte
@@ -29,7 +29,7 @@
 </script>
 
 <Select.Root type="single" onValueChange={(locale) => switchToLanguage(locale as Locale)}>
-	<Select.Trigger class="text-nav-text ">
+	<Select.Trigger class="text-nav-text [&_svg]:text-nav-text [&_svg]:opacity-100">
 		<span class="text-nav-text text-center">{labels[currentLanguage]}</span>
 	</Select.Trigger>
 	<Select.Content>
