5fbd6f59b0b00a6019924497f6bf46cb7e8aba15
Ross Wilson <ross.wilson@homeflow.co.uk>
2026-02-09 12:10:32 +0000

    221 user conversation preferences dto
    Actions:
    
    + create data transfer object for user conversation preferences routes
    + update frontend form to use camelCase values for user conversation
      preferences
    
    Motivation:
    
    + consistent standard using dtos for requests
    + js language standard

--- frontend/src/lib/components/UserConversationPreferencesForm/schema.ts
+++ frontend/src/lib/components/UserConversationPreferencesForm/schema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const userConversationPreferencesSchema = z.object({
-	receive_updates_by_notification: z.boolean(),
-	receive_updates_by_email: z.boolean(),
-	receive_similar_conversation_updates_by_email: z.boolean(),
-	receive_similar_conversation_updates_by_notification: z.boolean(),
+	receiveUpdatesByNotification: z.boolean(),
+	receiveUpdatesByEmail: z.boolean(),
+	receiveSimilarConversationUpdatesByEmail: z.boolean(),
+	receiveSimilarConversationUpdatesByNotification: z.boolean(),
 });
 
 export type UserConversationPreferencesFormData = z.infer<typeof userConversationPreferencesSchema>;
