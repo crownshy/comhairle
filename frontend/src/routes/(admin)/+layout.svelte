03575fd75b411acfcb6091825fc147980e08ebfa
Daniela Todorova <43169425+dtodorowa@users.noreply.github.com>
2026-02-16 09:38:41 +0000

    243 admin nav: AdminNav redesign, admin layout with SideBar.Provider, sidebar width

--- frontend/src/routes/(admin)/+layout.svelte
+++ frontend/src/routes/(admin)/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import AdminNav from '$lib/components/AdminNav.svelte';
+	import * as SideBar from '$lib/components/ui/sidebar';
 	import type { LayoutProps } from './$types';
 	import { page } from '$app/state';
 	import { loginRedirect } from '$lib/urls';
@@ -19,9 +20,11 @@
 	let themeCss = Object.entries(theme).reduce((a, b) => (a = a + `--${b[0]} : ${b[1]};`), '');
 </script>
 
-<div style={themeCss} class="bg-grey-100 grid h-full w-full flex-grow grid-cols-[400px_1fr]">
-	<AdminNav user={data.user} {conversations} path={page.url.pathname} />
-	<main class="flex flex-grow flex-col overflow-y-auto">
-		{@render children()}
-	</main>
+<div style={themeCss}>
+	<SideBar.Provider>
+		<AdminNav user={data.user} {conversations} path={page.url.pathname} />
+		<main class="flex flex-grow flex-col overflow-y-auto">
+			{@render children()}
+		</main>
+	</SideBar.Provider>
 </div>
