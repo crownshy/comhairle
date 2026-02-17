e365f405d7cd0384854895f133615bc3ea79ae3f
Daniela Todorova <43169425+dtodorowa@users.noreply.github.com>
2026-02-17 16:52:56 +0000

    redesign conversation card layout to match design

--- frontend/src/routes/(admin)/admin/+page.svelte
+++ frontend/src/routes/(admin)/admin/+page.svelte
@@ -4,12 +4,13 @@
 	import { Plus } from 'lucide-svelte';
 	import type { PageProps } from './$types';
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb';
+	import { Home } from 'lucide-svelte';
 
 	let props: PageProps = $props();
 	let conversations = props.data.conversations;
 </script>
 
-<div class="flex w-full flex-col justify-between gap-11 border-b-black bg-white px-16 py-8">
+<div class="flex w-full flex-col justify-between gap-11 border-b-black bg-white px-4 py-6 sm:px-8 md:px-16 md:py-8">
 	
 	<Breadcrumb.Root>
 		<Breadcrumb.List>
@@ -21,12 +22,14 @@
 		</Breadcrumb.List>
 	</Breadcrumb.Root>
 
-	<div class="mb-10 flex flex-row items-center justify-between">
-		<h1 class="text-4xl">Your conversations</h1>
-		<Button variant="default" href="/admin/conversations/new"><Plus />Create New Conversation</Button
-		>
+	<div class="mb-10 flex flex-col gap-4 items-start lg:justify-between lg:flex-row">
+		<div class="flex gap-2 items-center">	
+			<Home class="size-7 sm:size-9"/>
+			<h1 class="text-2xl sm:text-4xl">Your conversations</h1>
+		</div>
+		<Button class="w-full sm:w-auto" variant="default" href="/admin/conversations/new"><Plus />Create New Conversation</Button>
 	</div>
-	<div class=" grid w-full grid-cols-1 gap-x-2 gap-y-5 overflow-y-auto">
+	<div class="grid w-full grid-cols-1 gap-x-2 gap-y-5 overflow-y-auto">
 		{#each conversations.records as conversation (conversation.id)}
 			<a href={`/admin/conversations/${conversation.id}/configure`}>
 				<ConversationCard {conversation} />
