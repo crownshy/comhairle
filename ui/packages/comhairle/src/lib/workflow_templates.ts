aec90a3294581f0a6dcd87cc61750143aa8ffcc8
Ross Wilson <ross.wilson@homeflow.co.uk>
2026-03-12 13:00:41 +0000

    68 updates to elicitation bot text
    Actions:
    
    + provide functionality so that different steps can have different
      default name / description
    
    Motivation:
    
    + better default text for elicitation bot

--- ui/packages/comhairle/src/lib/workflow_templates.ts
+++ ui/packages/comhairle/src/lib/workflow_templates.ts
@@ -1,5 +1,13 @@
 import type { LocalizedConversationDto } from './api/api';
 
+export const defaultStepCreationParams: { [key: string]: { name: string; description: string } } = {
+	'Elicitation Bot': {
+		name: 'What do you think?',
+		description:
+			'Explore your opinions with the elicitation bot. The elicitation bot is designed to ask questions that help you explore and shape your views and opinions on a given topic.'
+	}
+};
+
 export const basic_learn_config = {
 	type: 'learn',
 	pages: [
