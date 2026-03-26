4081635397b4e00e8efab3f2570480f2fc4500e3
Ross Wilson <ross.wilson@homeflow.co.uk>
2026-03-26 16:58:30 +0000

    145 transcription worker setup
    Actions:
    
    + add apalis_redis and apalis_workflow
    + setup job queue for transcription worker using redis storage
    + add redis url to config
    
    Motivation:
    
    + new worker queue required for video call transcription processing
    + redis storage required for workflows

--- api/src/workers.rs
+++ api/src/workers.rs
@@ -1,13 +1,18 @@
 pub mod process_documents;
+pub mod process_video_call_transcriptions;
 
 use std::sync::Arc;
 
 use apalis::prelude::MemoryStorage;
+use apalis_redis::RedisStorage;
 use tokio::sync::Mutex;
 
-use crate::workers::process_documents::DocumentJob;
+use crate::workers::{
+    process_documents::DocumentJob, process_video_call_transcriptions::VideoCallJob,
+};
 
 #[derive(Clone, Debug)]
 pub struct JobQueues {
     pub process_documents: Arc<Mutex<MemoryStorage<DocumentJob>>>,
+    pub process_transcriptions: Arc<Mutex<RedisStorage<VideoCallJob>>>,
 }
