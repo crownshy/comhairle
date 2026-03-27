6651bfce523c3bb4a9d452eb1c9fd6ce7d675f58
Ross Wilson <ross.wilson@homeflow.co.uk>
2026-03-27 16:22:31 +0000

    145 stepped background worker
    Actions:
    
    + create initial stepped workflow for transcription background worker
    + route for beginning transcription worker
    + enable tracing on transcription worker
    
    Motivation:
    
    + transcription job would be better organised as stepped, sequential
      workflow

--- api/src/workers.rs
+++ api/src/workers.rs
@@ -3,16 +3,14 @@ pub mod process_video_call_transcriptions;
 
 use std::sync::Arc;
 
-use apalis::prelude::MemoryStorage;
+use apalis::prelude::{MemoryStorage, StepRequest};
 use apalis_redis::RedisStorage;
 use tokio::sync::Mutex;
 
-use crate::workers::{
-    process_documents::DocumentJob, process_video_call_transcriptions::VideoCallJob,
-};
+use crate::workers::process_documents::DocumentJob;
 
 #[derive(Clone, Debug)]
 pub struct JobQueues {
     pub process_documents: Arc<Mutex<MemoryStorage<DocumentJob>>>,
-    pub process_transcriptions: Arc<Mutex<RedisStorage<VideoCallJob>>>,
+    pub process_transcriptions: Arc<Mutex<RedisStorage<StepRequest<Vec<u8>>>>>,
 }
--- api/src/workers/process_video_call_transcriptions.rs
+++ api/src/workers/process_video_call_transcriptions.rs
@@ -1,7 +1,87 @@
+use apalis::prelude::*;
+use chrono::Utc;
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tracing::info;
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct VideoCallJob {
-    event_id: Uuid,
+use crate::{
+    error::ComhairleError,
+    models::job::{self, UpdateJob},
+    ComhairleState,
+};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct TranscribeRecording {
+    pub event_id: Uuid,
+    pub job_id: Uuid,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct UploadTranscription {
+    pub transcription_id: Uuid,
+    pub job_id: Uuid,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct RunSenseMaking {
+    pub transcription_id: Uuid,
+    pub job_id: Uuid,
+}
+
+pub async fn transcribe_recording(
+    req: TranscribeRecording,
+    _ctx: Data<()>,
+) -> Result<GoTo<UploadTranscription>, ComhairleError> {
+    info!(
+        event_id = %req.event_id,
+        job_id = %req.job_id,
+        "Starting transcription processing job"
+    );
+
+    info!("Read video call recording from file system and begin transcription");
+
+    Ok::<_, _>(GoTo::Next(UploadTranscription {
+        transcription_id: req.event_id,
+        job_id: req.job_id,
+    }))
+}
+
+pub async fn upload_transcription(
+    req: UploadTranscription,
+    _ctx: Data<()>,
+) -> Result<GoTo<RunSenseMaking>, ComhairleError> {
+    info!(
+        transcription_id = %req.transcription_id,
+        job_id = %req.job_id,
+        "Upload transcription via bulk upload service"
+    );
+
+    Ok::<_, _>(GoTo::Next(RunSenseMaking {
+        transcription_id: req.transcription_id,
+        job_id: req.job_id,
+    }))
+}
+
+pub async fn run_sense_making(
+    req: RunSenseMaking,
+    state: Data<Arc<ComhairleState>>,
+) -> Result<GoTo<&'static str>, ComhairleError> {
+    info!(
+        transcription_id = %req.transcription_id,
+        job_id = %req.job_id,
+        "Run video transcription through sense making service"
+    );
+
+    let update_job = UpdateJob {
+        status: Some("completed".to_string()),
+        finished_at: Some(Utc::now()),
+        completion_message: Some("Recording successully transcribed".to_string()),
+        ..Default::default()
+    };
+    let _ = job::update(&state.db, &req.job_id, update_job).await?;
+
+    Ok::<_, _>(GoTo::Done(
+        "Completed video call transcription job successfully",
+    ))
 }
