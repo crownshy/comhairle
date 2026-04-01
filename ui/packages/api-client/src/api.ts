ef9ebd9ea76901f13e04f1651cece8c091be7556
Ross Wilson <ross.wilson@homeflow.co.uk>
2026-04-01 14:22:27 +0100

    176 import conversation
    Actions:
    
    + route to create new conversation from imported json file
    + admin button to import conversation json file and upload
    
    Motivation:
    
    + allow importing of conversation from exported json

--- ui/packages/api-client/src/api.ts
+++ ui/packages/api-client/src/api.ts
@@ -535,7 +535,6 @@ export const ImportExportConversationDto = z
     description: z.string(),
     faqs: z.union([z.string(), z.null()]).optional(),
     imageUrl: z.string(),
-    isComplete: z.boolean(),
     isInviteOnly: z.boolean(),
     isLive: z.boolean(),
     isPublic: z.boolean(),
@@ -2591,6 +2590,22 @@ Use query param withUserProgress&#x3D;true to get the active user&#x27;s progres
       },
     ],
     response: WorkflowStepDto,
+  },
+  {
+    method: "post",
+    path: "/conversation/import",
+    alias: "postConversationimport",
+    description: `Imports a conversation from an exported json file`,
+    requestFormat: "form-data",
+    parameters: [
+      {
+        name: "body",
+        description: `multipart form data`,
+        type: "Body",
+        schema: z.array(z.any()),
+      },
+    ],
+    response: z.null(),
   },
   {
     method: "get",
