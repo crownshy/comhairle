17204c7db6c5529e191b03b43ce64e6cb0d0789d
Ross Wilson <ross.wilson@homeflow.co.uk>
2026-04-07 15:28:21 +0100

    176 remove api client ts compilation
    Actions:
    
    + remove ts compilation from build command in api client
    + remove dist folder
    
    Motivation:
    
    + exporting source ts files so compilation unnecessary

--- ui/packages/api-client/dist/client.d.ts
+++
@@ -1,3 +0,0 @@
-import { createApiClient as createApi } from './api';
-export declare const createApiClient: (baseUrl: string, authToken: string | undefined, source: string, locale?: string) => ReturnType<typeof createApi>;
-export declare const apiClient: ReturnType<typeof createApiClient>;
