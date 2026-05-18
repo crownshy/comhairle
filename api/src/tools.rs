035708bc7c4f8e80532f611c68fff8ea8a78d77e
Ross Wilson <ross.wilson@homeflow.co.uk>
2026-05-18 11:56:24 +0100

    343 prioritization tool setup
    Actions:
    
    + create new tool type for prioritization tool including config, setup
      and report
    
    Motivation:
    
    + prioritization tool build

--- api/src/tools.rs
+++ api/src/tools.rs
@@ -14,12 +14,14 @@ pub mod heyform;
 pub mod id;
 pub mod learn;
 pub mod polis;
+pub mod prioritization;
 pub mod stories;
 
 use elicitation_bot::{ElicitationBotReport, ElicitationBotToolConfig, ElicitationBotToolSetup};
 use heyform::{HeyFormReport, HeyFormToolConfig, HeyFormToolSetup};
 use learn::{LearnReport, LearnToolConfig, LearnToolSetup};
 use polis::{PolisReport, PolisToolConfig, PolisToolSetup};
+use prioritization::{PrioritizationReport, PrioritizationToolConfig, PrioritizationToolSetup};
 use stories::{StoriesReport, StoriesToolConfig, StoriesToolSetup};
 
 /// Core trait that all tools must implement.
@@ -105,6 +107,7 @@ pub enum ToolConfig {
     HeyForm(HeyFormToolConfig),
     Stories(StoriesToolConfig),
     ElicitationBot(ElicitationBotToolConfig),
+    Prioritization(PrioritizationToolConfig),
 }
 
 impl ToolConfig {
@@ -117,6 +120,9 @@ impl ToolConfig {
             ToolConfig::Stories(config) => stories::StoriesTool::sync_data(config, state).await,
             ToolConfig::ElicitationBot(config) => {
                 elicitation_bot::ElicitationBotTool::sync_data(config, state).await
+            }
+            ToolConfig::Prioritization(config) => {
+                prioritization::PrioritizationTool::sync_data(config, state).await
             }
         }
     }
@@ -139,6 +145,9 @@ impl ToolConfig {
             ToolConfig::ElicitationBot(config) => Ok(ToolConfig::ElicitationBot(
                 elicitation_bot::ElicitationBotTool::clone_tool(config, state).await?,
             )),
+            ToolConfig::Prioritization(config) => Ok(ToolConfig::Prioritization(
+                prioritization::PrioritizationTool::clone_tool(config, state).await?,
+            )),
         }
     }
 
@@ -151,6 +160,9 @@ impl ToolConfig {
             ToolConfig::Stories(config) => stories::StoriesTool::delete(config, state).await,
             ToolConfig::ElicitationBot(config) => {
                 elicitation_bot::ElicitationBotTool::delete(config, state).await
+            }
+            ToolConfig::Prioritization(config) => {
+                prioritization::PrioritizationTool::delete(config, state).await
             }
         }
     }
@@ -172,6 +184,9 @@ impl ToolConfig {
             ToolConfig::ElicitationBot(config) => {
                 elicitation_bot::ElicitationBotTool::register_workers(config, state).await
             }
+            ToolConfig::Prioritization(config) => {
+                prioritization::PrioritizationTool::register_workers(config, state).await
+            }
         }
     }
 }
@@ -184,6 +199,7 @@ pub enum ToolSetup {
     HeyForm(HeyFormToolSetup),
     Stories(StoriesToolSetup),
     ElicitationBot(ElicitationBotToolSetup),
+    Prioritization(PrioritizationToolSetup),
 }
 
 impl ToolSetup {
@@ -205,6 +221,9 @@ impl ToolSetup {
             ToolSetup::ElicitationBot(setup) => Ok(ToolConfig::ElicitationBot(
                 elicitation_bot::ElicitationBotTool::setup(setup, state).await?,
             )),
+            ToolSetup::Prioritization(setup) => Ok(ToolConfig::Prioritization(
+                prioritization::PrioritizationTool::setup(setup, state).await?,
+            )),
         }
     }
 }
@@ -217,6 +236,7 @@ pub fn router(state: Arc<ComhairleState>) -> ApiRouter {
         .merge(heyform::HeyFormTool::routes(&state))
         .merge(stories::StoriesTool::routes(&state))
         .merge(elicitation_bot::ElicitationBotTool::routes(&state))
+        .merge(prioritization::PrioritizationTool::routes(&state))
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone, JsonSchema)]
@@ -226,4 +246,5 @@ pub enum ReportConfig {
     Learn(LearnReport),
     Stories(StoriesReport),
     ElicitationBot(ElicitationBotReport),
+    Prioritization(PrioritizationReport),
 }
