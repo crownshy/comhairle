228a18fc0e29ce52ec2f6f53c8af6c7f0cd80394
Ross Wilson <rosswilson2306@gmail.com>
2026-06-18 13:16:32 +0100

    430 admin email vars popover
    Actions:
    
    + refactor EmailTemplateVariables component to use popover for
      explanation
    
    Motivation:
    
    + explanation takes up too much space and could be annoying once you are
      used to the system

--- ui/packages/comhairle/src/routes/(admin)/admin/email-template-configs/EmailTemplateVariables.svelte
+++ ui/packages/comhairle/src/routes/(admin)/admin/email-template-configs/EmailTemplateVariables.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+	import * as Popover from '$lib/components/ui/popover';
+	import { Info } from 'lucide-svelte';
+
 	type Props = { templateVariables: string[] };
 	let { templateVariables }: Props = $props();
+
+	let open = $state(false);
 </script>
 
 {#snippet quote(message: string)}
@@ -9,25 +14,36 @@
 	</blockquote>
 {/snippet}
 
-<div class="flex max-w-4xl flex-col gap-8">
-	<div class="flex flex-col gap-2">
-		<h2 class="text-lg font-bold">Personalise your email content</h2>
-		<p>
-			The fields below support dynamic <span class="font-semibold italic">variables</span>,
-			which are automatically replaced with real values when the email is sent. To use a
-			variable, include it in your content wrapped in double curly braces.
-		</p>
-	</div>
+<div class="flex flex-col gap-8">
+	<Popover.Root bind:open>
+		<Popover.Trigger class="flex justify-start">
+			<span class="flex items-center gap-2 font-bold">
+				Available variables <Info class="text-primary inline size-5 self-center" />
+			</span>
+		</Popover.Trigger>
+		<Popover.Content class="max-w-[70vw] overflow-y-auto" side="bottom" align="start">
+			<div class="flex flex-col gap-2">
+				<h2 class="text-lg font-bold">Personalise your email content</h2>
+				<p>
+					The fields below support dynamic <span class="font-semibold italic"
+						>variables</span
+					>, which are automatically replaced with real values when the email is sent. To
+					use a variable, include it in your content wrapped in double curly braces.
+				</p>
+			</div>
+
+			<div class="flex flex-col gap-2">
+				<p>For example, if <code>conversation_title</code> is available, writing:</p>
+				{@render quote('You are invited to take part in {{conversation_title}}.')}
+				<p>will be sent to recipients as:</p>
+				{@render quote(
+					'You are invited to take part in Town Centre Regeneration Consultation.'
+				)}
+			</div>
+		</Popover.Content>
+	</Popover.Root>
 
 	<div class="flex flex-col gap-2">
-		<p>For example, if <code>conversation_title</code> is available, writing:</p>
-		{@render quote('You are invited to take part in {{conversation_title}}.')}
-		<p>will be sent to recipients as:</p>
-		{@render quote('You are invited to take part in Town Centre Regeneration Consultation.')}
-	</div>
-
-	<div class="flex flex-col gap-2">
-		<h3 class="font-bold">Available variables</h3>
 		<p>The following variables can be used in any content field for this email:</p>
 		<ul class="list-inside list-disc">
 			{#each templateVariables as templateVar (templateVar)}
