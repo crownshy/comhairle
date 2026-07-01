359e353fdb56081f3ae147903295ceb05dac9d1e
ADSmith-0 <adam@crown-shy.com>
2026-07-01 18:04:28 +0100

    Moved data to the backend

--- ui/packages/comhairle/src/routes/(admin)/admin/media-library/+page.server.ts
+++ ui/packages/comhairle/src/routes/(admin)/admin/media-library/+page.server.ts
@@ -2,7 +2,36 @@ import { tryFetch } from '$lib/utils/errorHandling';
 import { fail } from '@sveltejs/kit';
 import type { RequestEvent } from './$types';
 import Media from '$lib/interfaces/Media';
+import type { MediaDto } from '@crownshy/api-client/api';
+import climateUk from '$lib/assets/climageuk.jpg';
+import beepBox from '$lib/assets/BeepBox-Song.mp3';
+import troublshootingVideo from '$lib/assets/(before)cannot_unlink.mp4';
+import vtaiwan from '$lib/assets/vtaiwan.jpg';
+import seattleUSA from '$lib/assets/seattle_usa.jpg';
+import wavesLogoLg from '$lib/assets/waves-logo-lg.png';
+import comhairleLogo from '$lib/assets/comhairle_logo.png';
+import placeholderConvo from '$lib/assets/placeholder_convo.png';
+import comhairleFullLogo from '$lib/assets/comhairle_full_logo.svg';
 
+type ContentType = MediaDto['contentType'] | 'audio/mp3';
+type MockData = Pick<MediaDto, 'id'> & Record<'src', string> & Record<'contentType', ContentType>;
+export function load() {
+	const media: MockData[] = [
+		{ id: '1', src: climateUk, contentType: 'image/jpeg' },
+		{ id: '2', src: beepBox, contentType: 'audio/mp3' },
+		{ id: '3', src: troublshootingVideo, contentType: 'video/mp4' },
+		{ id: '4', src: vtaiwan, contentType: 'image/jpeg' },
+		{ id: '5', src: seattleUSA, contentType: 'image/jpeg' },
+		{ id: '6', src: wavesLogoLg, contentType: 'image/png' },
+		{ id: '7', src: comhairleLogo, contentType: 'image/png' },
+		{ id: '8', src: placeholderConvo, contentType: 'image/png' },
+		{ id: '9', src: comhairleFullLogo, contentType: 'image/png' }
+	];
+
+	return {
+		media
+	};
+}
 export const actions = {
 	upload: async ({ request, fetch }: RequestEvent) => {
 		const data = await request.formData();
--- ui/packages/comhairle/src/routes/(admin)/admin/media-library/+page.svelte
+++ ui/packages/comhairle/src/routes/(admin)/admin/media-library/+page.svelte
@@ -2,37 +2,21 @@
 	import { enhance } from '$app/forms';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { SquarePen, Trash2, X } from 'lucide-svelte';
-	import climateUk from '$lib/assets/climageuk.jpg';
-	import vtaiwan from '$lib/assets/vtaiwan.jpg';
-	import seattleUSA from '$lib/assets/seattle_usa.jpg';
-	import wavesLogoLg from '$lib/assets/waves-logo-lg.png';
-	import comhairleLogo from '$lib/assets/comhairle_logo.png';
-	import placeholderConvo from '$lib/assets/placeholder_convo.png';
-	import comhairleFullLogo from '$lib/assets/comhairle_full_logo.svg';
 	import DeleteDialog from './DeleteDialog.svelte';
 	import { flip } from 'svelte/animate';
 	import { MediaUpload } from '$lib/components/Media';
 	import { m } from '$lib/paraglide/messages';
 	import { notifications } from '$lib/notifications.svelte';
+	import MediaItem from '$lib/components/Media/MediaItem.svelte';
 
 	let deleteForm: HTMLFormElement | undefined;
 
 	let bulkEdit = $state<boolean>(false);
 	let selected = $state([]);
 
-	const images: { id: number; src: string }[] = [
-		{ id: 1, src: climateUk },
-		{ id: 2, src: vtaiwan },
-		{ id: 3, src: seattleUSA },
-		{ id: 4, src: wavesLogoLg },
-		{ id: 5, src: comhairleLogo },
-		{ id: 6, src: placeholderConvo },
-		{ id: 7, src: comhairleFullLogo }
-	];
-
 	const ROW_HEIGHT = 30;
 
-	const { form } = $props();
+	const { form, data } = $props();
 
 	$effect(() => {
 		if (form?.failures) {
@@ -89,7 +73,7 @@
 	<main class="mt-5">
 		<form method="POST" action="?/delete" use:enhance bind:this={deleteForm}>
 			<ul class="flex flex-wrap gap-2">
-				{#each images as image (image.id)}
+				{#each data.media as item (item.id)}
 					<li
 						class={`relative h-[${ROW_HEIGHT}vh] grow overflow-hidden rounded-sm`}
 						animate:flip={{ duration: 100 }}
@@ -99,7 +83,7 @@
 								<input
 									type="checkbox"
 									name="media"
-									value={image.id.toString()}
+									value={item.id.toString()}
 									class="accent-primary absolute top-2 left-2 z-2 h-4 cursor-pointer"
 									bind:group={selected}
 								/>
