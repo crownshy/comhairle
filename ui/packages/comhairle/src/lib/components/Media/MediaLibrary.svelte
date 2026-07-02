42e57dbc0d934736a0fd4dd23e4b9b30efec579e
ADSmith-0 <adam@crown-shy.com>
2026-07-02 15:45:54 +0100

    Added text if no media was found

--- ui/packages/comhairle/src/lib/components/Media/MediaLibrary.svelte
+++ ui/packages/comhairle/src/lib/components/Media/MediaLibrary.svelte
@@ -13,14 +13,20 @@
 </script>
 
 <ul class="flex flex-wrap gap-2">
-	{#each data as d (d.id)}
-		{@const type = htmlFromMediaType(d.contentType)}
-		{#if type}
-			<li class={`relative h-[${rowHeight}vh] grow overflow-hidden rounded-sm`}>
-				{@render media(type, d)}
-			</li>
-		{/if}
-	{/each}
+	{#if data.length === 0}
+		<div class="mt-8">
+			<p class="text-muted-foreground font-bold">No media found</p>
+		</div>
+	{:else}
+		{#each data as d (d.id)}
+			{@const type = htmlFromMediaType(d.contentType)}
+			{#if type}
+				<li class={`relative h-[${rowHeight}vh] grow overflow-hidden rounded-sm`}>
+					{@render media(type, d)}
+				</li>
+			{/if}
+		{/each}
+	{/if}
 </ul>
 
 <style>
