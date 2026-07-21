e9ff83a8f63bc646e9bf36121facf9516cdcaac9
Ross Wilson <rosswilson2306@gmail.com>
2026-07-21 15:57:27 +0100

    669 thinking space summary modal
    Actions:
    
    + add dialog to expand thinking space summary and questions for insights
      table
    
    Motivation:
    
    + expanded insights data

--- ui/packages/comhairle/src/lib/reports/thinking-space/InsightsTable.svelte
+++ ui/packages/comhairle/src/lib/reports/thinking-space/InsightsTable.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import * as Table from '$lib/components/ui/table';
+	import * as Dialog from '$lib/components/ui/dialog';
 	import type { ThinkingSpaceUserInsights } from '@crownshy/api-client/api';
+	import { CornerDownRight } from '@lucide/svelte';
 
 	type Props = {
 		userInsights: ThinkingSpaceUserInsights[];
 	};
 	let { userInsights }: Props = $props();
+
+	let selectedUserInsights = $state<ThinkingSpaceUserInsights | null>(null);
+	let openDialog = $derived(selectedUserInsights !== null);
 </script>
 
 <Table.Root class="w-full table-fixed">
@@ -18,7 +23,11 @@
 	</Table.Header>
 	<Table.Body>
 		{#each userInsights as insight (insight.userId)}
-			<Table.Row>
+			<Table.Row
+				onclick={() =>
+					(selectedUserInsights =
+						userInsights.find((i) => i.userId === insight.userId) ?? null)}
+			>
 				<Table.Cell class="text-muted-foreground border-l-6 px-5 py-9"
 					>{insight.userId.substring(0, 8).concat('...')}</Table.Cell
 				>
@@ -40,3 +49,45 @@
 		{/each}
 	</Table.Body>
 </Table.Root>
+
+<Dialog.Root bind:open={openDialog}>
+	<Dialog.Content class="flex h-[70vh] w-full max-w-6xl flex-col gap-8 sm:w-full sm:max-w-6xl">
+		<Dialog.Header>
+			<Dialog.Title class="text-2xl">Participant #{selectedUserInsights?.userId}</Dialog.Title
+			>
+			<Dialog.Description class="text-muted-foreground text-sm"
+				>Thinking space summary, follow-up questions and responses</Dialog.Description
+			>
+		</Dialog.Header>
+		<div class="">
+			<h3 class="mb-4 font-bold">Summary</h3>
+			<p>{selectedUserInsights?.summary.summary}</p>
+		</div>
+		<div class="overflow-y-auto">
+			<h3 class="mb-4 font-bold">Root questions (and responses)</h3>
+			<div class="flex flex-col gap-8">
+				{#each selectedUserInsights?.answers as answer (answer.root.id)}
+					<div class="my-4 flex flex-col gap-4">
+						<p class="text-primary">{answer.root.question}</p>
+						<p class="flex items-center gap-4">
+							<CornerDownRight /><span>{answer.root.answer}</span>
+						</p>
+						<div class="mt-8 flex flex-col gap-4">
+							<h4 class="flex items-center gap-4 font-bold">
+								<CornerDownRight /> Follow-up questions (and responses)
+							</h4>
+							<div class="flex flex-col gap-4 pl-8">
+								{#each answer.followUps as followUp (followUp.id)}
+									<p class="text-primary">{followUp.question}</p>
+									<p class="flex items-center gap-4">
+										<CornerDownRight /><span>{followUp.answer}</span>
+									</p>
+								{/each}
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+		</div>
+	</Dialog.Content>
+</Dialog.Root>
