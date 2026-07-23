6b6724178ae6aa468ebc27ec12bf8df533bc284a
ADSmith-0 <adam00199382818@gmail.com>
2026-07-23 09:55:11 +0100

    Fixed key handler and styling

--- ui/packages/comhairle/src/lib/components/FileUpload.svelte
+++ ui/packages/comhairle/src/lib/components/FileUpload.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { FileText } from 'lucide-svelte';
 	import { Button } from '$lib/components/ui/button';
-	import { notifications } from '$lib/notifications.svelte';
 
 	type Props = {
 		onfile: (file: File) => Promise<void>;
@@ -16,7 +15,6 @@
 
 	type State = 'idle' | 'dragging' | 'uploading' | 'error';
 	let status = $state<State>('idle');
-	let errorMessage = $state<string>('');
 
 	const acceptedExtensions = $derived(accept?.split(',') ?? []);
 	const maxSizeBytes = $derived(maxSizeMB ? maxSizeMB * 1_024 * 1_024 : undefined);
@@ -62,19 +60,19 @@
 			for (const file of files) {
 				if (maxSizeBytes && file.size > maxSizeBytes) {
 					status = 'error';
-					errorMessage = 'Max file size exceeded';
+					fileInput?.setCustomValidity('Max file size exceeded');
 					return;
 				}
 
 				const extension = file.name.match(/\..*/)?.[0];
 				if (!extension) {
 					status = 'error';
-					errorMessage = "Couldn't recognise file type";
+					fileInput?.setCustomValidity("Couldn't recognise file type");
 					return;
 				}
 				if (!acceptedExtensions.includes(extension)) {
 					status = 'error';
-					errorMessage = 'File type not supported';
+					fileInput?.setCustomValidity('File type not supported');
 					return;
 				}
 
@@ -85,68 +83,71 @@
 	}
 
 	$effect(() => {
-		fileInput?.setCustomValidity('bad!');
+		console.log('message:', fileInput?.validationMessage);
 	});
 </script>
 
-<div class="flex w-full flex-col gap-2 py-5 lg:flex-row lg:justify-between">
-	<div
-		role="button"
-		tabindex="0"
-		class="border-input dark:bg-input/30 flex w-full cursor-pointer flex-col items-center gap-4 rounded-xl border bg-gray-50 p-8 transition-colors"
-		class:bg-gray-100={status === 'dragging'}
-		class:border-primary={status === 'dragging'}
-		ondrop={(event) => {
-			event.preventDefault();
-			status = 'idle';
-			handleFiles(event.dataTransfer?.files);
-		}}
-		ondragover={(event) => {
-			event.preventDefault();
-			status = 'dragging';
-		}}
-		ondragleave={(event) => {
-			event.preventDefault();
-			status = 'idle';
-		}}
-		onkeydown={(event) => {
-			if (event.key !== 'Enter') {
-				fileInput?.click();
-			}
-		}}
-	>
-		<div class="h-8 w-8 text-gray-400">
-			<FileText class="h-full w-full" />
-		</div>
-		<div class="flex flex-col items-center gap-2">
-			<div class="text-foreground text-center text-base font-medium">
-				{status === 'dragging' ? 'Drop your files here' : 'Drag and drop your files'}
-			</div>
-			{#if inputMessage}
-				<div class="text-muted-foreground text-center text-sm">
-					{inputMessage}
-				</div>
-			{/if}
-		</div>
-		<Button
-			variant="outline"
-			onclick={() => fileInput?.click()}
-			disabled={status === 'uploading'}
-		>
-			{status === 'uploading' ? 'Uploading...' : 'Select file'}
-		</Button>
-		<input
-			bind:this={fileInput}
-			type="file"
-			{accept}
-			{multiple}
-			class="hidden"
-			onchange={(event) => {
-				handleFiles((event.target as HTMLInputElement).files);
-			}}
-		/>
+<div
+	role="button"
+	tabindex="0"
+	class="border-input dark:bg-input/30 flex w-full cursor-pointer flex-col items-center gap-4 rounded-xl border bg-gray-50 p-8 py-5 transition-colors"
+	class:bg-gray-100={status === 'dragging'}
+	class:border-primary={status === 'dragging'}
+	ondrop={(event) => {
+		event.preventDefault();
+		status = 'idle';
+		handleFiles(event.dataTransfer?.files);
+	}}
+	ondragover={(event) => {
+		event.preventDefault();
+		status = 'dragging';
+	}}
+	ondragleave={(event) => {
+		event.preventDefault();
+		status = 'idle';
+	}}
+	onkeydown={(event) => {
+		if (event.key !== 'Enter') {
+			return;
+		}
+		fileInput?.click();
+	}}
+>
+	<div class="h-8 w-8 text-gray-400">
+		<FileText class="h-full w-full" />
 	</div>
+	<div class="flex flex-col items-center gap-2">
+		<div class="text-foreground text-center text-base font-medium">
+			{status === 'dragging' ? 'Drop your files here' : 'Drag and drop your files'}
+		</div>
+		{#if inputMessage}
+			<div class="text-muted-foreground text-center text-sm">
+				{inputMessage}
+			</div>
+		{/if}
+		{#if fileInput?.validity.valid === false}
+			<div class="text-destructive text-center text-sm">
+				{fileInput.validationMessage}
+			</div>
+		{/if}
+	</div>
+	<Button variant="outline" onclick={() => fileInput?.click()} disabled={status === 'uploading'}>
+		{status === 'uploading' ? 'Uploading...' : 'Select file'}
+	</Button>
+	<input
+		bind:this={fileInput}
+		type="file"
+		{accept}
+		{multiple}
+		class="hidden"
+		onchange={(event) => {
+			handleFiles((event.target as HTMLInputElement).files);
+		}}
+	/>
 </div>
 
 <style>
+	div[role='button']:has(input:user-invalid) {
+		border-color: oklch(from var(--color-destructive) l c h / 0.8);
+	}
 </style>
