5041593b12ed07d59dc95658688c469c818f4ba5
ADSmith-0 <adam00199382818@gmail.com>
2026-08-13 11:31:24 +0100

    Added initial file reading logic

--- ui/packages/translator/src/main.rs
+++ ui/packages/translator/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use core::error;
+use std::{collections::HashMap, env, fs, io};
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let main_language = get_main_language();
+    let file = get_translation(main_language.as_str())?;
+
+    for key in file.keys() {
+        println!("{}: {}", key, file[key]);
+    }
+
+    Ok(())
+}
+
+fn get_main_language() -> String {
+    env::var("MAIN_LANGUAGE").unwrap_or(String::from("en"))
+}
+
+fn get_translation(language: &str) -> Result<HashMap<String, String>, io::Error> {
+    let contents = fs::read_to_string(format!("../comhairle/messages/{language}.json"))?;
+    let json = serde_json::from_str(contents.as_str())?;
+    Ok(json)
 }
