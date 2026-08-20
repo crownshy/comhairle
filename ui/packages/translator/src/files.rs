5a8d542ddc49ddf886909a6d7c0587b9732072bb
ADSmith-0 <adam00199382818@gmail.com>
2026-08-20 22:18:07 +0100

    Added reading the directory and file

---
+++ ui/packages/translator/src/files.rs
@@ -0,0 +1,79 @@
+use std::{
+    collections::BTreeMap,
+    ffi::OsString,
+    fs,
+    path::{Path, PathBuf},
+};
+
+const UNSAFE_LANGUAGES_DIR: &str = "../comhairle/messages/";
+
+pub struct LanguageFiles;
+impl LanguageFiles {
+    fn dir() -> PathBuf {
+        Path::new(UNSAFE_LANGUAGES_DIR).to_path_buf()
+    }
+
+    fn path(language: &str) -> PathBuf {
+        Path::new(UNSAFE_LANGUAGES_DIR).join(format!("{language}.json"))
+    }
+
+    fn languages<'a>()
+    -> Result<impl Iterator<Item = Result<Option<OsString>, std::io::Error>>, std::io::Error> {
+        let dir = fs::read_dir(Path::new(UNSAFE_LANGUAGES_DIR))?;
+        let languages =
+            dir.map(|entry| entry.map(|e| e.path().file_prefix().map(|p| p.to_owned())));
+        Ok(languages)
+    }
+}
+// Export block
+pub fn languages()
+-> Result<impl Iterator<Item = Result<Option<OsString>, std::io::Error>>, std::io::Error> {
+    LanguageFiles::languages()
+}
+// End export block
+
+pub fn read(language: &str) -> Result<BTreeMap<String, String>, std::io::Error> {
+    let contents = fs::read_to_string(LanguageFiles::path(language))?;
+    let json: BTreeMap<String, String> = serde_json::from_str(contents.as_str())?;
+    Ok(json)
+}
+
+pub fn write(language: &str) -> Result<BTreeMap<String, String>, std::io::Error> {
+    let contents =
+        fs::read_to_string(Path::new(&format!("../comhairle/messages/{language}.json")))?;
+    let json: BTreeMap<String, String> = serde_json::from_str(contents.as_str())?;
+    Ok(json)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_get_dir() {
+        let dir = LanguageFiles::dir();
+        let Some(dir) = dir.to_str() else {
+            panic!("Couldn't convert dir");
+        };
+        assert_eq!(dir, "../comhairle/messages/");
+    }
+
+    #[test]
+    fn should_get_path() {
+        let file = LanguageFiles::path("en");
+        let Some(file) = file.to_str() else {
+            panic!("Couldn't convert path");
+        };
+        assert_eq!(file, "../comhairle/messages/en.json");
+    }
+
+    #[test]
+    fn should_get_all_languages() {
+        let paths = LanguageFiles::languages();
+        let Ok(paths) = paths else {
+            panic!("Couldn't read directory");
+        };
+        let paths: Vec<Result<OsString, std::io::Error>> = paths.collect();
+        assert!(paths.len() > 1);
+    }
+}
--- ui/packages/translator/src/main.rs
+++ ui/packages/translator/src/main.rs
@@ -1,12 +1,37 @@
-use translator::languages;
+use translator::{files, languages};
 
 fn main() -> Result<(), Box<dyn core::error::Error>> {
-    let main_language = languages::get_main_language();
-    let file = languages::get_file(&main_language)?;
+    // let main_language = languages::get_main_language();
+    // let file = files::read(&main_language)?;
 
-    for key in file.keys() {
-        println!("{}: {}", key, file[key]);
+    for language in files::languages()? {
+        let language = match language {
+            Ok(l) => l,
+            Err(e) => {
+                eprintln!("{e}");
+                continue;
+            }
+        };
+
+        let Some(language) = language else {
+            eprintln!("Could not find file name");
+            continue;
+        };
+
+        let language = match language.into_string() {
+            Ok(l) => l,
+            Err(e) => {
+                eprintln!("{e:?}");
+                continue;
+            }
+        };
+
+        println!("{language}");
     }
+
+    // for key in file.keys() {
+    //     println!("{}: {}", key, file[key]);
+    // }
 
     Ok(())
 }
