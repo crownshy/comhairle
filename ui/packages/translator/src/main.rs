26ea72e61976f1830b945938535bf300d7bb1ea7
ADSmith-0 <adam00199382818@gmail.com>
2026-08-20 15:26:36 +0100

    Changed to use Path

--- ui/packages/translator/src/main.rs
+++ ui/packages/translator/src/main.rs
@@ -1,5 +1,5 @@
 use core::error;
-use std::{collections::BTreeMap, env, fs, io};
+use std::{collections::BTreeMap, env, fs, io, path::Path};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let main_language = get_main_language();
@@ -17,7 +17,7 @@ fn get_main_language() -> String {
 }
 
 fn get_translation(language: &str) -> Result<BTreeMap<String, String>, io::Error> {
-    let contents = fs::read_to_string(format!("../comhairle/messages/{language}.json"))?;
+    let contents = fs::read_to_string(Path::new(&format!("../comhairle/messages/{language}.json")))?;
     let json: BTreeMap<String, String> = serde_json::from_str(contents.as_str())?;
     Ok(json)
 }
