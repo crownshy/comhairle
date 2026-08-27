ff9961f5c6834dd7827af5aef2089204c72948b7
Ross Wilson <rosswilson2306@gmail.com>
2026-08-27 09:10:13 +0100

    947 refresh token route
    Actions:
    
    + endpoint for refreshing user session and rotating refresh token
    + db method for revoking all refresh tokens in a family group
    + db method for rotating refresh tokens
    + error variant for refresh failure reason
    
    Motivation:
    
    + keep user sessions alive without continual logins
    + revoke all tokens in family if reuse detected, signalling token
      compromised
    + keep attach surface low by rotating refresh tokens when used

--- ui/packages/api-client/src/api.ts
+++ ui/packages/api-client/src/api.ts
@@ -3752,6 +3752,14 @@ const endpoints = makeApi([
     response: z.void(),
   },
   {
+    method: "get",
+    path: "/auth/refresh",
+    alias: "RefreshSession",
+    description: `Refresh user session to prevent frequent users logging back in`,
+    requestFormat: "json",
+    response: UserDto,
+  },
+  {
     method: "post",
     path: "/auth/resend_verification_email",
     alias: "ResendVerificationEmail",
