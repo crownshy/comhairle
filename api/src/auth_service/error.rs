73415eb5d14b635fac88bfa76018698da5e7c098
Ross Wilson <rosswilson2306@gmail.com>
2026-09-10 10:27:12 +0100

    1029 keycloak user sync
    Actions:
    
    + add AuthService method to create user with implementation for keycloak
    + add endpoint to sync comhairle users to keycloak
    + helper function to extract password metadata from phc hash for
      keycloak credentials sync
    
    Motivation:
    
    + sync comhairle users into keycloak

--- api/src/auth_service/error.rs
+++ api/src/auth_service/error.rs
@@ -6,6 +6,15 @@ use thiserror::Error;
 pub enum AuthServiceError {
     #[error("Keycloak error: {0}")]
     KeycloakError(#[from] keycloak::KeycloakError),
+
+    #[error("Access token failure: {0}")]
+    AccessTokenFailure(String),
+
+    #[error("Sync user error: {0}")]
+    SyncUserError(String),
+
+    #[error("Credential extraction failure: {0}")]
+    CredentialExtractionFailure(String),
 }
 
 // TODO: status code implementation
