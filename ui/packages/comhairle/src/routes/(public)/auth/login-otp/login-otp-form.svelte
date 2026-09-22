6e4dc53446841e2e59a1758bcea7098208b9646f
ADSmith-0 <adam00199382818@gmail.com>
2026-09-22 12:14:12 +0100

    Updated login pages

--- ui/packages/comhairle/src/routes/(public)/auth/login-otp/login-otp-form.svelte
+++ ui/packages/comhairle/src/routes/(public)/auth/login-otp/login-otp-form.svelte
@@ -12,6 +12,7 @@
 	import { resolve } from '$app/paths';
 	import { Spinner } from '$lib/components/ui/spinner';
 	import { onMount } from 'svelte';
+	import { key } from '$lib/utils/invalidationKey';
 
 	let { backTo }: { backTo?: string } = $props();
 
@@ -63,7 +64,7 @@
 						} catch {}
 					}
 
-					await goto(resolve(redirectTo), { invalidateAll: true });
+					await goto(resolve(redirectTo), { invalidate: [key('user')] });
 				} catch (e) {
 					responseMessage = e.response.data.err;
 				}
